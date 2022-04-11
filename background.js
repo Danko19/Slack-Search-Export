0ad7421e93ff76fd3737e72cd08a5c17b0dd577c
Daniil Mardanov <wally19@bk.ru>
2022-04-11 23:17:00 +0400

    refactoring. rewrite recursion to async/await

--- background.js
+++ background.js
@@ -1,12 +1,15 @@
-var onBeforeRequestListener = function(details) {
+async function onBeforeRequestListener(details) {
 	if (!details.requestBody.formData?.module?.includes('messages') 
 		|| details.requestBody.formData?.automatic?.includes('true'))
 		return;
-	console.log(details);
 
+	const messages = await fetchAllMessages(details);
+	console.log(messages);
+}
+
+async function fetchAllMessages(details){
 	const apiUrl = details.url.match('.*(?=search\.modules)')[0];
-	console.log(apiUrl);
-
+	
 	const formData = new FormData();
 	formData.append('token', details.requestBody.formData.token[0]);
 	formData.append('query', details.requestBody.formData.query[0]);
@@ -17,27 +20,24 @@ var onBeforeRequestListener = function(details) {
 	
 	var messages = [];
 	var total = 0;
-	var page = 1;
+	var pageNumber = 1;
 	
-	FetchAllMessages().then(() => console.log(messages));
-	
-	function FetchAllMessages(){
-		formData.set('page', page++);
-		return fetch(apiUrl + 'search.modules', {
+	do{
+		formData.set('page', pageNumber++);
+		var page = await fetch(apiUrl + 'search.modules', {
 			method: 'POST',
 			body: formData,
 			credentials: 'include'
 		}).then(function(r) {
 			return r.json();
 		}).then(function(data) {
-			for(var item of data.items)
-				messages.push(item.messages[0]);
-			if (messages.length < data.pagination.total_count)
-				return FetchAllMessages();
+			total = data.pagination.total_count;
+			return data.items.map(item => item.messages[0]);
 		});		
-	}
+		messages = messages.concat(page);
+	} while (messages.length < total);
 	
-	function 
+	return messages;
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
